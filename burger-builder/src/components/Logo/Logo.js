75b649016a653ead6f56bb1bf93e58832c5e3b38
Sivajiganesh Kandimalla <Sivajiganesh.Kandimalla2@T-mobile.com>
2018-06-04 15:02:04 -0700

    Adding Navigation Components

--- burger-builder/src/components/Logo/Logo.js
+++ burger-builder/src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
-import React from'react';
-import classes from './Logo.css'
-import burgerLogo from '../../assets/images/burger-logo.png'
+import React from 'react';
 
-const logo =(props)=>(
-    <div className={classes.Logo}>
-        <img src={burgerLogo} alt="BurgerLogo"/>
+import burgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.css';
+
+const logo = (props) => (
+    <div className={classes.Logo} style={{height: props.height}}>
+        <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
 
